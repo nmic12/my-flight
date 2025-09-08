9ba69b1a4990e8828445bea63deb64e8cf91d9e7
nmic <nicolas.michels12@gmail.com>
2025-09-08 11:48:46 -0300

    1.3.1 funcoes adicionadas em gerenciadorcias

--- consoleApp/App.java
+++ consoleApp/App.java
@@ -1,9 +1,15 @@
 package pucrs.myflight.consoleApp;
 
+import pucrs.myflight.modelo.GerenciadorCias;
+
 public class App {
 
 	public static void main(String[] args) {
 		System.out.println("\nMyFlight project...");
-
+		GerenciadorCias gCias = new GerenciadorCias();
+		gCias.addCia("8J2", "JOAOAIRLINES");
+		gCias.get_cias();
+		gCias.search_by_code();
+		gCias.serach_by_name();
 	}
 }
--- modelo/GerenciadorCias.java
+++ modelo/GerenciadorCias.java
@@ -1,11 +1,50 @@
 package pucrs.myflight.modelo;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GerenciadorCias {
 	private ArrayList<CiaAerea> empresas;
-	
+	Scanner cin = new Scanner(System.in);
+
 	public GerenciadorCias() {
 		empresas = new ArrayList<>();
 	}
+
+	public void addCia(String codigo, String nome) {
+		empresas.add(new CiaAerea(codigo, nome));
+	}
+
+	public void get_cias() {
+		for (CiaAerea empresa : empresas) {
+			System.out.println(empresa.getNome());
+			System.out.println(empresa.getCodigo());
+		}
+	}
+
+	public void search_by_code() {
+		System.out.println("Insira o código: ");
+		String codigo = cin.nextLine();
+		for (CiaAerea empresa : empresas) {
+			if (empresa.getCodigo().equalsIgnoreCase(codigo)) {
+				System.out.println("Empresa Encontrada: " + empresa.getNome());
+			} else {
+				System.out.println("Erro, nenhuma empresa foi encontrada.");
+			}
+		}
+
+	}
+
+	public void serach_by_name() {
+		System.out.println("Insira o nome: ");
+		String nome = cin.nextLine();
+		for (CiaAerea empresa : empresas) {
+			if (empresa.getNome().equalsIgnoreCase(nome)) {
+				System.out.println("Empresa Encontrada: " + empresa.getCodigo());
+			} else {
+				System.out.println("Erro, nenhuma empresa foi encontrada.");
+			}
+		}
+	}
+
 }
